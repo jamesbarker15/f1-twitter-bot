8071582088294bc9610b687308ea65249af6bba6
jamesbarker <jamesbarker15@aol.com>
2023-06-24 22:00:46 +0100

    Add exception to deal with error codes relating to older data.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests, random, tweepy
 import keys
 
+
 # Functions
 def get_api():
     auth = tweepy.OAuthHandler(keys.api_key, keys.api_secret)
@@ -17,14 +18,34 @@ def get_client():
     return client
 
 
-def post_tweet(api: tweepy.API, client:tweepy.Client, message):
+def post_tweet(api: tweepy.API, client: tweepy.Client, message):
     client.create_tweet(text=message)
     print('Posted tweet')
 
 
-year = random.randrange(1990, 2022)
+# Select a year between 1950 and 2022 at random
+year = random.randrange(1950, 2022)
 
-if year in [1990, 1991, 1992, 1993, 1994, 1996, 1998, 1999, 2003]:
+# Select a race number, dependent on how many races there were that year.
+if year in [1950, 1955]:
+    race = random.randrange(1, 7)
+elif year in [1951, 1952, 1956, 1957, 1961]:
+    race = random.randrange(1, 8)
+elif year in [1953, 1954, 1959, 1962, 1966]:
+    race = random.randrange(1, 9)
+elif year in [1960, 1963, 1964, 1965]:
+    race = random.randrange(1, 10)
+elif year in [1958, 1967, 1969, 1971]:
+    race = random.randrange(1, 11)
+elif year in [1968, 1972]:
+    race = random.randrange(1, 12)
+elif year in [1970]:
+    race = random.randrange(1, 13)
+elif year in [1975, 1980]:
+    race = random.randrange(1, 14)
+elif year in [1973, 1974, 1979, 1981, 1983]:
+    race = random.randrange(1, 15)
+elif year in [1990, 1991, 1992, 1993, 1994, 1996, 1998, 1999, 2003]:
     race = random.randrange(1, 16)
 elif year in [1995, 1997, 2000, 2001, 2002, 2007, 2009, 2020]:
     race = random.randrange(1, 17)
@@ -39,7 +60,7 @@ elif year in [2016, 2018, 2019]:
 else:
     race = random.randrange(1, 22)
 
-url = f"http://ergast.com/api/f1/{year}/{race}/results.json"
+url = f"https://ergast.com/api/f1/{year}/{race}/results.json"
 
 # Request the data
 request = requests.get(url)
@@ -60,19 +81,28 @@ winner = results[0]
 driver = winner["Driver"]
 constructor = winner["Constructor"]
 
-# Create the tweet.
-message = "The " + circuit['raceName'] + " in the " + circuit['season'] + \
-          " season took place on " + circuit['date'] + " at the " \
-          + circuit_name["circuitName"] + ", the winning driver was " \
-          + driver['givenName'] + " " + driver['familyName'] + \
-          " - " + driver['code'] + " racing for " + constructor['name'] + ". "
+# Create the tweet, the using try and except for older data which doesn't
+# contain driver codes
+try:
+    message = "The " + circuit['raceName'] + " in the " + circuit['season'] + \
+              " season took place on " + circuit['date'] + " at the " \
+              + circuit_name["circuitName"] + ", the winning driver was " \
+              + driver['givenName'] + " " + driver['familyName'] + \
+              " - " + driver['code'] + " racing for " + constructor[
+                  'name'] + ". "
+except:
+    message = "The " + circuit['raceName'] + " in the " + circuit['season'] + \
+              " season took place on " + circuit['date'] + " at the " \
+              + circuit_name["circuitName"] + ", the winning driver was " \
+              + driver['givenName'] + " " + driver['familyName'] + \
+              " racing for " + constructor[
+                  'name'] + ". "
 
 # Post the tweet
 if __name__ == '__main__':
     api = get_api()
     client = get_client()
     post_tweet(api, client, message)
-
 
 
 
